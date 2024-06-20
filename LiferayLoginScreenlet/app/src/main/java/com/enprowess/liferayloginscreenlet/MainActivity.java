2bc872be42350c4ab7750e0d09b0f5b33c8ddd4d
N1khil <saininikhilnnn@gmail.com>
2024-06-20 12:04:29 +0530

    also added the toast notification for login success and failure with redirection.

--- LiferayLoginScreenlet/app/src/main/java/com/enprowess/liferayloginscreenlet/MainActivity.java
+++ LiferayLoginScreenlet/app/src/main/java/com/enprowess/liferayloginscreenlet/MainActivity.java
@@ -2,43 +2,55 @@ package com.enprowess.liferayloginscreenlet;
 
 import static android.content.ContentValues.TAG;
 
-import android.nfc.Tag;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.liferay.mobile.screens.auth.login.LoginListener;
+import com.liferay.mobile.screens.auth.login.LoginScreenlet;
 import com.liferay.mobile.screens.context.User;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_screenlet), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        LoginScreenlet loginScreenlet = (LoginScreenlet) findViewById(R.id.login_screenlet);
+
+        loginScreenlet.setListener(this);
     }
 
 
+    @Override
     public void onLoginSuccess(User user) {
-        Log.i(TAG,"Login Successfully");
-        Toast toast = Toast.makeText(getApplicationContext(),"Login Successfully",Toast.LENGTH_SHORT);
-        toast.show();
+        Log.i(TAG, user.getEmail() + " " + user.getScreenName() + " " + user.getValues());
+
+        // Create an intent to start ActivityOne
+        Intent intent = new Intent(MainActivity.this, ActivitySuccess.class);
+
+        // Put the user details into the intent
+        intent.putExtra("userEmail", user.getEmail());
+        intent.putExtra("userScreenName", user.getScreenName());
+
+        // Start ActivityOne
+        startActivity(intent);
+
+        // Show the Toast message
+        Toast.makeText(MainActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
     }
 
-    public void onLoginFailure(User user) {
-        Log.i(TAG,"Login Failed");
-        Toast toast = Toast.makeText(getApplicationContext(),"Login Failed",Toast.LENGTH_SHORT);
-        toast.show();
+    @Override
+    public void onLoginFailure(Exception e) {
+        Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAuthenticationBrowserShown() {
+
     }
 }
